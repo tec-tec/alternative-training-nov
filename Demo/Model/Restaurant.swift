bb0b588e9decf1008c5b0a77636d9ab67683a920
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-11-23 12:01:35 +0100

    Added a Restaurant class model

---
+++ Demo/Model/Restaurant.swift
@@ -0,0 +1,26 @@
+//
+//  Restaurant.swift
+//  Demo
+//
+//  Created by Ludovic Ollagnier on 23/11/2015.
+//  Copyright © 2015 Tec-Tec. All rights reserved.
+//
+
+import UIKit
+
+struct Restaurant {
+
+    enum RestoType: String {
+        case Unknown
+        case FastFood
+        case Indian
+        case Japanese
+        case French
+    }
+
+    var name: String
+    var type: RestoType
+    var adress: String
+    var grade: Float
+    var comment: String
+}
