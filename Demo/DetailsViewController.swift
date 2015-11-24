0ffbf03664921a5dfd368d39b927b84d84d7d211
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-11-24 12:13:41 +0100

    Added basic UI for details

--- Demo/DetailsViewController.swift
+++ Demo/DetailsViewController.swift
@@ -7,30 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailsViewController: UIViewController {
 
     var aRestaurant: Restaurant!
+
+    @IBOutlet weak var restaurantAddressLabel: UILabel!
+    @IBOutlet weak var restaurantGradeLabel: UILabel!
+    @IBOutlet weak var restaurantCommentTextView: UITextView!
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        configureUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func configureUI() {
+        restaurantAddressLabel.text = aRestaurant.adress
+        restaurantCommentTextView.text = aRestaurant.comment
+        restaurantGradeLabel.text = "\(aRestaurant.grade)"
+        title = aRestaurant.name
     }
-    */
-
 }
