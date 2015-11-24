a87bdb49b5668a4672f96f75bb49eca184ec24d3
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-11-24 12:14:09 +0100

    Added a comment for all the fakeRestaurants

--- Demo/RestaurantManager.swift
+++ Demo/RestaurantManager.swift
@@ -24,11 +24,11 @@ class RestaurantManager {
 
     func createFakeRestaurants() {
 
-        restaurantArray.append(Restaurant(name: "Test", type: Restaurant.RestoType.Indonesian, adress: "10 Rue de la pompe, 75015 Paris", grade: 5, comment: ""))
-        restaurantArray.append(Restaurant(name: "Test2", type: Restaurant.RestoType.Indonesian, adress: "11 Rue de la pompe, 75015 Paris", grade: 5, comment: ""))
-        restaurantArray.append(Restaurant(name: "Test3", type: Restaurant.RestoType.Indonesian, adress: "12 Rue de la pompe, 75015 Paris", grade: 5, comment: ""))
-        restaurantArray.append(Restaurant(name: "Test4", type: Restaurant.RestoType.Indonesian, adress: "13 Rue de la pompe, 75015 Paris", grade: 5, comment: ""))
-        restaurantArray.append(Restaurant(name: "Test5", type: Restaurant.RestoType.Indonesian, adress: "10 Rue de la pompe, 75015 Paris", grade: 5, comment: ""))
+        restaurantArray.append(Restaurant(name: "Test", type: Restaurant.RestoType.Indonesian, adress: "10 Rue de la pompe, 75015 Paris", grade: 5, comment: "Test de commentaire pour un restaurant. C'était très bon !"))
+        restaurantArray.append(Restaurant(name: "Test2", type: Restaurant.RestoType.Indonesian, adress: "11 Rue de la pompe, 75015 Paris", grade: 5, comment: "Test de commentaire pour un restaurant. C'était très bon !"))
+        restaurantArray.append(Restaurant(name: "Test3", type: Restaurant.RestoType.Indonesian, adress: "12 Rue de la pompe, 75015 Paris", grade: 5, comment: "Test de commentaire pour un restaurant. C'était très bon !"))
+        restaurantArray.append(Restaurant(name: "Test4", type: Restaurant.RestoType.Indonesian, adress: "13 Rue de la pompe, 75015 Paris", grade: 5, comment: "Test de commentaire pour un restaurant. C'était très bon !"))
+        restaurantArray.append(Restaurant(name: "Test5", type: Restaurant.RestoType.Indonesian, adress: "10 Rue de la pompe, 75015 Paris", grade: 5, comment: "Test de commentaire pour un restaurant. C'était très bon !"))
     }
 
     func addRestaurant(resto: Restaurant) {
